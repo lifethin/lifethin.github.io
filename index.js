991280cf2485c7d0e8ccdd12ddf69e80337271fd
awchjimmy <awchjimmy@gmail.com>
2020-07-28 10:44:49 +0800

    change layout las-light-theme

--- index.js
+++ index.js
@@ -1,18 +1,47 @@
+Vue.component("app-top-navbar", {
+  template: `<nav class="navbar sticky-top navbar-expand-sm" id="app-top-navbar">
+  
+</nav>`,
+});
+
+Vue.component("app-fixed-buttom-navbar", {
+  template: `<nav class="navbar fix-buttom navbar-expand-sm row" id="app-fixed-buttom-navbar">
+  <div class="col-4"></div>
+  
+  <a class="col-4" href="https://lihi1.com/4bBiU" target="_blank">
+    <img src="images/fathersday-baoming.svg" style="width: 100%;">
+  </a>
+  <a class="col-4" href="https://lihi1.com/9sdiA" target="_blank">
+    <img src="images/fathersday-dianhua.svg" style="width: 100%;">
+  </a>
+
+
+  </div>
+</nav>`,
+});
+
+Vue.component("app-static-content", {
+  template: `<div id="app-static-content">
+  <img src="images/0728-phone-01.jpg">
+  <img src="images/0728-phone-02.jpg">
+  </div>`,
+});
+
 const dgiApp = new Vue({
   el: "#dgi-app",
-  data: function() {
+  data: function () {
     return {
-      isMobile: true
+      isMobile: true,
     };
   },
   mounted() {
     this.checkIsMobile();
   },
   methods: {
-    checkIsMobile: function() {
+    checkIsMobile: function () {
       if (navigator.userAgent.indexOf("obile") === -1) {
         this.isMobile = false;
       }
-    }
-  }
+    },
+  },
 });
