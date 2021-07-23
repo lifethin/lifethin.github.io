9ca6a3a2bd9b0163a486a0f54667e2b225baa386
kevinlin919 <kevinlin919@gmail.com>
2021-07-24 02:12:51 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 Vue.component("app-static-content", {
   template: `
   <div class="app_wrapper">
-    <header id="app-top-navbar" style="height: 7px;">
-      <v-toolbar>
+    <header>
+      <v-toolbar id="app-top-navbar" style="height: 7px;">
         <v-tabs v-model="tab" class="top-menu-tab">
           <v-tab key="Main">首頁</v-tab>
           <v-tab key="Three">餐點介紹</v-tab>
@@ -10,8 +10,8 @@ Vue.component("app-static-content", {
         </v-tabs>
       </v-toolbar>
     </header>
-    <main style="padding: 0px;">
-      <div id="app-static-content">
+    <main>
+      <div id="app-static-content" style="padding: 0px;">
         <v-tabs grow="grow" background-color="#3c5c1d" dark v-model="tab" class="menu-tab">
           <v-tab key="Main">首頁</v-tab>
           <v-tab key="Three">餐點介紹</v-tab>
