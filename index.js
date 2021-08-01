9c544e182e59059ee6e43af002b2f9c770bac12c
kevinlin919 <kevinlin919@gmail.com>
2021-08-01 21:59:29 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ Vue.component("app-static-content", {
   <div class="app_wrapper">
     <header>
       <v-toolbar id="app-top-navbar" style="height: 7px;">
-        <div background-color="#3c5c1d" class="top-menu-tab">
-          <div class="link_main">首頁</div>
-          <div class="link_menu">餐點介紹</div>
-          <div class="link_party">派對外燴服務</div>
-        </div>
+        <v-tabs background-color="#3c5c1d" class="top-menu-tab">
+          <v-tab key="Main">首頁</v-tab>
+          <v-tab key="Menu">餐點介紹</v-tab>
+          <v-tab key="Party">派對外燴服務</v-tab>
+        </v-tabs>
         <div class="toolbar-links">
           <button>
               <img src="images/button_call.png" />
@@ -23,11 +23,11 @@ Vue.component("app-static-content", {
     </header>
     <main>
       <div id="app-static-content" style="padding: 0px;">
-        <div background-color="#3c5c1d" class="menu-tab">
-          <div class="link_main">首頁</div>
-          <div class="link_menu">餐點介紹</div>
-          <div class="link_party">派對外燴服務</div>
-        </div>
+        <v-tabs grow dark background-color="#3c5c1d" class="menu-tab">
+          <v-tab key="Main">首頁</v-tab>
+          <v-tab key="Menu">餐點介紹</v-tab>
+          <v-tab key="Party">派對外燴服務</v-tab>
+        </v-tabs>
         <v-tabs-items v-model="tab" class="content-area">
           <v-tab-item key="Main">
             <picture>
@@ -35,13 +35,13 @@ Vue.component("app-static-content", {
               <img src="images/20210714_home_phone.jpg?ver=1.3" />
             </picture>
           </v-tab-item>
-          <v-tab-item key="Three">
+          <v-tab-item key="Menu">
             <picture>
               <source media="(min-width: 465px)" srcset="images/20210714_menu_desktop.jpg?ver=1.3" />
               <img src="images/20210714_menu_phone.jpg?ver=1.3" />
             </picture>
           </v-tab-item>
-          <v-tab-item key="Four">
+          <v-tab-item key="Party">
             <picture>
               <source media="(min-width: 465px)" srcset="images/20210714_party_desktop.jpg?ver=1.3" />
               <img src="images/20210714_party_phone.jpg?ver=1.3" />
