7d8ddb24d4d3afdedea57f2bd20abfe91a5339cc
kevinlin919 <kevinlin919@gmail.com>
2021-08-02 00:24:42 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,70 +1,64 @@
-Vue.component("app-static-content", {
-  template: `
-  <div class="app_wrapper">
-    <header>
-      <v-toolbar id="app-top-navbar" style="height: 7px;">
-        <v-tabs v-model="mainTab" class="top-menu-tab">
-          <v-tab key="Main" @click="updateRoute('')">首頁</v-tab>
-          <v-tab key="Menu" @click="updateRoute('menu')">餐點介紹</v-tab>
-          <v-tab key="Party" @click="updateRoute('partyservice')">派對外燴服務</v-tab>
-        </v-tabs>
-        <div class="toolbar-links">
-          <button>
-              <img src="images/button_call.png" />
-          </button>
-          <button>
-              <img src="images/button_facebook.png" />
-          </button>
-          <button>
-              <img src="images/button_line.png" />
-          </button>
-        </div>
-      </v-toolbar>
-    </header>
-    <main>
-      <div id="app-static-content" style="padding: 0px;">
-        <v-tabs grow="grow" background-color="#3c5c1d" dark v-model="mainTab" class="menu-tab">
-          <v-tab key="Main" @click="updateRoute('')">首頁</v-tab>
-          <v-tab key="Menu" @click="updateRoute('menu')">餐點介紹</v-tab>
-          <v-tab key="Party" @click="updateRoute('partyservice')">派對外燴服務</v-tab>
-        </v-tabs>
-        <v-tabs-items v-model="mainTab" class="content-area">
-          <v-tab-item key="Main">
-            <picture>
-              <source media="(min-width: 465px)" srcset="images/20210714_home_desktop.jpg?ver=1.3" />
-              <img src="images/20210714_home_phone.jpg?ver=1.3" />
-            </picture>
-          </v-tab-item>
-          <v-tab-item key="Menu">
-            <picture>
-              <source media="(min-width: 465px)" srcset="images/20210714_menu_desktop.jpg?ver=1.3" />
-              <img src="images/20210714_menu_phone.jpg?ver=1.3" />
-            </picture>
-          </v-tab-item>
-          <v-tab-item key="Party">
-            <picture>
-              <source media="(min-width: 465px)" srcset="images/20210714_party_desktop.jpg?ver=1.3" />
-              <img src="images/20210714_party_phone.jpg?ver=1.3" />
-            </picture>
-          </v-tab-item>
-        </v-tabs-items>
-      </div>
-    </main>
-  </div>
-  `,
-  data: function() {
-    return {
-      mainTab: null
-    };
-  }
-});
-
 const dgiApp = new Vue({
+  template: `
+  <v-app id="v-app">
+    <div class="app_wrapper">
+      <header>
+        <v-toolbar id="app-top-navbar" style="height: 7px;">
+          <v-tabs v-model="mainTab" class="top-menu-tab">
+            <v-tab key="Main" @click="updateRoute('')">首頁</v-tab>
+            <v-tab key="Menu" @click="updateRoute('menu')">餐點介紹</v-tab>
+            <v-tab key="Party" @click="updateRoute('partyservice')">派對外燴服務</v-tab>
+          </v-tabs>
+          <div class="toolbar-links">
+            <button>
+                <img src="images/button_call.png" />
+            </button>
+            <button>
+                <img src="images/button_facebook.png" />
+            </button>
+            <button>
+                <img src="images/button_line.png" />
+            </button>
+          </div>
+        </v-toolbar>
+      </header>
+      <main>
+        <div id="app-static-content" style="padding: 0px;">
+          <v-tabs grow="grow" background-color="#3c5c1d" dark v-model="mainTab" class="menu-tab">
+            <v-tab key="Main" @click="updateRoute('')">首頁</v-tab>
+            <v-tab key="Menu" @click="updateRoute('menu')">餐點介紹</v-tab>
+            <v-tab key="Party" @click="updateRoute('partyservice')">派對外燴服務</v-tab>
+          </v-tabs>
+          <v-tabs-items v-model="mainTab" class="content-area">
+            <v-tab-item key="Main">
+              <picture>
+                <source media="(min-width: 465px)" srcset="images/20210714_home_desktop.jpg?ver=1.3" />
+                <img src="images/20210714_home_phone.jpg?ver=1.3" />
+              </picture>
+            </v-tab-item>
+            <v-tab-item key="Menu">
+              <picture>
+                <source media="(min-width: 465px)" srcset="images/20210714_menu_desktop.jpg?ver=1.3" />
+                <img src="images/20210714_menu_phone.jpg?ver=1.3" />
+              </picture>
+            </v-tab-item>
+            <v-tab-item key="Party">
+              <picture>
+                <source media="(min-width: 465px)" srcset="images/20210714_party_desktop.jpg?ver=1.3" />
+                <img src="images/20210714_party_phone.jpg?ver=1.3" />
+              </picture>
+            </v-tab-item>
+          </v-tabs-items>
+        </div>
+      </main>
+    </div>
+  </v-app>
+  `,
   el: "#dgi-app",
   vuetify: new Vuetify(),
   data: function() {
     return {
-      mainTab: 0,
+      mainTab: 'Main',
       isMobile: true
     };
   },
